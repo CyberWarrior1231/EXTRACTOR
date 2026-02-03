33f2f97e049ecd6f1be9117bb48dd756c8f52da6
CyberWarrior1231 <thestaralwayad@gmail.com>
2026-02-03 15:35:50 +0530

    Update func.py

--- Extractor/core/func.py
+++ Extractor/core/func.py
@@ -61,7 +61,7 @@ async def subscribe(app, message):
         try:
             user = await app.get_chat_member(update_channel, message.from_user.id)
             if user.status == "kicked":
-                await message.reply_text("🚫 Sorry Sir, You are Banned. Contact My Support Group @ITsGOLU_OFFICIAL")
+                await message.reply_text("🚫 Sorry Sir, You are Banned. Contact My Support Group @Theprimevault01")
                 return 1
         except UserNotParticipant:
             try:
@@ -102,7 +102,7 @@ async def subscribe(app, message):
                 await message.reply_text(
                     "❗ Please join our updates channel to use the bot.",
                     reply_markup=InlineKeyboardMarkup([[
-                        InlineKeyboardButton("🤖 ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ 🤖", url="https://t.me/ITsGOLU_OFFICIAL")
+                        InlineKeyboardButton("🤖 ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ 🤖", url="https://t.me/Theprimevault01")
                     ]])
                 )
             return 1
